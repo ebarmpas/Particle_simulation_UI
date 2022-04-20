217982d2ce494c4b297918f9276ab39a1a40e5e8
ebarmpas <ebarmpas@citycollege.sheffield.eu>
2022-04-20 20:57:07 +0300

    Added dynamic vertical/horizontal scaling

--- src/edu/sheffield/dissertation/simulationUI/Main.java
+++ src/edu/sheffield/dissertation/simulationUI/Main.java
@@ -13,55 +13,52 @@ public class Main extends PApplet {
 	SimulationConfiguration simConf;
 	ButtonMap buttons;
 	
-	FileReader fileReader;
 	Thread fileReaderThread;
 	
 	List<Step> steps;
-	int currentStep, delay, maxDelay;
+	int currentStep;
+	
+	int smallSide;
 	
     public void setup(){
-        frameRate(60);
-        surface.setResizable(true);
-        surface.setLocation(0, 0);
+        frameRate(120);
         surface.setTitle("Particle Simulation");
-        
-        background(0);
-        
+                
         JFileChooser chooser = new JFileChooser();
         chooser.setFileFilter(new FileNameExtensionFilter(".psc files", "psc"));
         if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
         	try {
      			simConf = new SimulationConfiguration(chooser.getSelectedFile());
      		} catch (FileNotFoundException e) {
-     			e.printStackTrace();
-     			exit();
+     			simConf = null;
      		}
-                
-        } else
+        }
+        if(simConf != null) {
+        	                	
+        	surface.setTitle("Particle Simulation / " + simConf.getAppName());
+        	
+        	FileReader fileReader = new FileReader(simConf);
+	        fileReaderThread = new Thread(fileReader);
+	        fileReaderThread.start();
+	        steps = fileReader.getValue();
+	        	        
+	        buttons = new ButtonMap(this);
+	        buttons.addButton("paused",' ');
+	        buttons.addButton("rewind", 'r');
+
+	        currentStep = 0;
+	        	       
+	        if(width > height)
+	        	smallSide = height;
+	        else
+	        	smallSide = width;
+	        
+	        System.out.println(smallSide);
+	        
+	        colorMode(HSB, simConf.getSpeciesNumber(), 255, 255);
+	        rectMode(CORNER);
+        }else
         	exit();
-        
-        fileReader = new FileReader(simConf);
-        fileReaderThread = new Thread(fileReader);
-        
-        fileReaderThread.start();
-        
-        steps = fileReader.getValue();
-        
-        surface.setTitle("Particle Simulation / " + simConf.getAppName());
-        
-        
-        buttons = new ButtonMap(this);
-        buttons.addButton("paused",' ');
-        buttons.addButton("rewind", 'r');
-        
-        delay = 0;
-        maxDelay = 10;
-        currentStep = 0;
-
-        colorMode(HSB, simConf.getSpeciesNumber(), 255, 255);
-
-        
-        
     }
  
     public void draw(){
@@ -69,14 +66,24 @@ public class Main extends PApplet {
     if(frameCount % 3 == 0 && !buttons.isPressed("paused")) {
     	
     	background(0);
-    	strokeWeight(10);
+    	strokeWeight(5);
     	
     	List<Particle> p = steps.get(currentStep).getParticles();
-
+    	
     	for(int i = 0; i < p.size(); i++) {
-    		stroke(p.get(i).getSpecies(), 255 , 255);
-    		point(p.get(i).getLocation().x, p.get(i).getLocation().y);
+    		Particle part = p.get(i);   		
+    		stroke(part.getSpecies(), 255 , 255);
+    		point(map(part.getLocation().x, 0, 1000, 0, smallSide), map(part.getLocation().y, 0, 1000, 0, smallSide));
     	}
+    	
+    	stroke(255, 0, 255);
+    	strokeWeight(1);
+    	if(smallSide == height)
+    		line(smallSide, 0, smallSide + 1, smallSide);
+    	else
+    		line(0, smallSide, smallSide + 1, smallSide);
+    	
+    	
     	if(buttons.isPressed("rewind"))
     		if(currentStep == 0)
     			currentStep = steps.size() - 1;
@@ -88,16 +95,17 @@ public class Main extends PApplet {
     		else
     			currentStep++;
     	}
-
     }
 
     // The argument passed to main must match the class name
     public static void main(String[] args) {
+    	
         PApplet.main("edu.sheffield.dissertation.simulationUI.Main");
     }
 
     // method used only for setting the size of the window
     public void settings(){
-        size(displayWidth, displayHeight);
+        size((int)Math.round(displayWidth * 0.8), (int)Math.round(displayHeight * 0.8));
     }
+    
 }
